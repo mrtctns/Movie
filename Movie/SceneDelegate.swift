2aeb77430e1a18520b9c0648730b3da20d1a24a8
Mert Çetin <mrtctns22@gmail.com>
2024-02-26 01:54:59 +0300

    scene delegate güncellendi

--- Movie/SceneDelegate.swift
+++ Movie/SceneDelegate.swift
@@ -15,8 +15,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
-        let viewController = ViewController()
-        let navigation = UINavigationController(rootViewController: viewController)
+        let splash = SplashRouter.setupModule()
+        let navigation = UINavigationController(rootViewController: splash)
+        window.overrideUserInterfaceStyle = .dark
         window.rootViewController = navigation
         self.window = window
         window.makeKeyAndVisible()
